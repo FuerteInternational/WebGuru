10aceaf2aeb9fd9fa00f95cf6099f5f76c6ac87b
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2012-09-19 18:35:06 +0100

    new companies module + translations
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

---
+++ admin/system/companies/class.actions.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Action class for module Companies
+ * 
+ * @package      WebGuru3
+ * @subpackage   modules/companies/
+ * @author       Ondrej Rafaj
+ * @author       WebGuruCMS3 Framework CMS admin generator (http://www.webgurucms.com)
+ * @version      1.0.0.0
+ * @wgversion    3.0.0.0
+ * @wgdeveloper  1.0.0.1
+ * @since        19. September 2012
+ */
+
+class actionsCompanies extends actions {
+	
+	public function __construct($parameters) {
+		$func = array();
+		// SYSTEM: functions-list -------------------------------------------------------------
+		$func['doindexjob'] = array('doIndexJob', 'index');
+		$func['docompanieseditjob'] = array('doCompanieseditJob', 'companiesedit');
+		
+		// END: functions-list ----------------------------------------------------------------
+		$funcname = parent::_init($func, $parameters);
+		$ok = false;
+		if ((bool) $funcname && method_exists($this, $funcname)) $ok = $this->$funcname($parameters);
+		else parent::_reportError($funcname, __FILE__);
+		wgPaths::moduleRedirect(NULL, !$ok);
+	}
+	
+	// SYSTEM: functions ----------------------------------------------------------------------
+
+	/**
+	 * This is the _doIndexJob function description
+	 *
+	 * @name _doIndexJob
+	 * @author Ondrej Rafaj
+	 * @param array $parameters Params from url
+	 * @return bool Success
+	 */
+	private function _doIndexJob($parameters) {
+		require(wgPaths::getModulePath().'actions/class.index.php');
+		$class = new companiesActionsIndex();
+		if ((bool) $class->init()) { wgError::add('actionok', 2);
+			return true;
+		}
+		else { wgError::add('actionfailed');
+			return false;
+		}
+	}
+	
+	/**
+	 * This is the _doCompanieseditJob function description
+	 *
+	 * @name _doCompanieseditJob
+	 * @author Ondrej Rafaj
+	 * @param array $parameters Params from url
+	 * @return bool Success
+	 */
+	private function _doCompanieseditJob($parameters) {
+		require(wgPaths::getModulePath().'actions/class.companiesedit.php');
+		$class = new companiesActionsCompaniesedit();
+		if ((bool) $class->init()) { wgError::add('actionok', 2);
+			return true;
+		}
+		else { wgError::add('actionfailed');
+			return false;
+		}
+	}
+	
+	
+	
+	// END: functions ----------------------------------------------------------------------
+	
+}
+		
+?>
---
+++ admin/system/companies/pages/companiesedit.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Page companiesedit for module Companies
+ * 
+ * @package      WebGuru3
+ * @subpackage   modules/companies/pages/
+ * @author       Ondrej Rafaj
+ * @author       WebGuruCMS3 Framework CMS admin generator (http://www.webgurucms.com)
+ * @version      1.0.0.0
+ * @wgversion    3.0.0.0
+ * @wgdeveloper  1.0.0.1
+ * @since        19. September 2012
+ */
+
+$system['parse']['head'] = '';
+$system['parse']['editor'] = false;
+$tab = new myTabs('myTabs');
+
+// ----------- editcompanies (Block: editcompanies) start -----------
+$block = 'editcompanies';
+$tpl = new wgParse($temp, $path);
+$tpl->setCurrentBlock($block);
+
+$var['ACTIONNAME'] = 'companiesediteditcompanies';
+
+wgSystem::clearDefPostValue();
+
+wgSystem::defPostValue(CompaniesModel::COL_ID, '');
+wgSystem::defPostValue(CompaniesModel::COL_NAME, '');
+wgSystem::defPostValue(CompaniesModel::COL_IDENTIFIER, '');
+$lv = array();
+$item = new CompaniesModel();
+$item->setDefaultResults(wgSystem::getPost());
+
+//$arr = CompaniesModel::getSelfPagerData(pager::getPage($block), 20);
+$arr = CompaniesModel::doPager(array(), pager::getPage($block));
+// CompaniesModel::getPagerData(pager::getPage($block), 0, 20);
+if (!empty($arr['data']) && is_array($arr['data'])) foreach ($arr['data'] as $val) {
+	$tpl->setCurrentBlock('listeditcompanies');
+	$lv['LID'] = $val->getId();
+	$lv['LNAME'] = $val->getName();
+	$lv['LIDENTIFIER'] = $val->getIdentifier();
+	$lv['EDITROW'] = wgIcons::getButton('edit', $val->getName(), wgPaths::makeTableEditLink($val->getId(), 'show='.$var['ACTIONNAME']));
+	$lv['DELETEROW'] = wgIcons::getButton('delete', $val->getName(), wgPaths::makeTableDeleteLink($val->getId(), 'act='.$var['ACTIONNAME']), true);
+	$tpl->setVariable($lv);
+	$tpl->parseBlock('listeditcompanies');
+	if (wgSystem::getRequestValue('edit') == $val->getId() && wgSystem::getRequestValue('show') == $var['ACTIONNAME']) $item = $val;
+}
+$var['DATAPAGER'] = pager::makeAdminPager($arr['pager']);
+$lv = array();
+
+$var['COLID'] = $item->getId();
+$var['COLNAME'] = $item->getName();
+$var['COLIDENTIFIER'] = $item->getIdentifier();
+
+$var = wgSystem::getFormCallback($var);
+
+$tpl->setVariable($var);
+$tpl->parseBlock($block);
+$tab->addTab('editcompanies', wgLang::get('editcompanies'), true, $tpl->getBlock($block));
+// ----------- editcompanies end -----------
+
+
+
+$var = array();
+$system['parse']['content'] = $tab->getAll();
+?>
