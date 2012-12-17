c524121f00278dc7471349c37ccabbc7d1f888a9
jakub.rafaj <jakub.rafaj@fuerteint.com>
2012-12-17 16:09:44 +0100

    xmas folder

---
+++ xmas/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="generator" content="PSPad editor, www.pspad.com">
+  <link media="all" href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css' />
+  <link media="Screen" href="style.css" type="text/css" rel="stylesheet" />
+  <title>Fuerte</title>
+  </head>
+  <body>
+    <div id="container">
+      <div id="content_container">
+        <div id="content">
+          <div id="vimeo">
+            <iframe src='http://player.vimeo.com/video/55607969' width='532' height='393' frameborder='0'></iframe>
+<!--             <iframe src='http://player.vimeo.com/video/55607969' width='532' height='400' frameborder='0'></iframe>
+ --><!--             <iframe src='http://player.vimeo.com/video/55607969' width='605' height='455' frameborder='0'></iframe> -->
+<!--             <iframe src='http://player.vimeo.com/video/55607969' width='564' height='423' frameborder='0'></iframe> -->
+          </div>
+          <div id="text1">
+            Merry Christmas to you<br />
+            and Appy new Year
+          </div>
+          <a href="http://www.fuerteint.com" id="fuerte_logo">
+            &nbsp;
+          </a>
+          <div id="text2">
+            For all our new and existing customers, we would like to offer <a href="http://www.fuerteint.com">5% off</a> the final<br />
+            price of your app if you contact us before the end of the year.<br />
+            Be sure to use discount code 'FlurryFurdinand' to receive your gift. 
+          </div>
+          <div id="text3">
+            Furdinand and his team are already preparing for the upcoming year,<br />
+            with lots of exciting ideas to make this an appy new year.<br />
+            Get in touch with us now to be first to hear about it!
+          </div>
+        </div>
+        <div id="content_footer">
+          &nbsp;
+        </div>
+        <a id="visit_us" href="http://www.fuerteint.com">
+          Visit our Website
+        </a>
+      </div>
+      <div id="footer">
+        <div id="footer_texts">
+          <div id="copyright">©&nbsp;2012&nbsp;</div>
+          <a id="footer_texts_c" href="http://www.fuerteint.com">
+            Fuerte International
+          </a>
+          <a id="footer_texts_lin" href="http://www.linkedin.com/company/fuerte-international/">
+            &nbsp;
+          </a>
+          <a id="footer_texts_t" href="http://www.twitter.com/fuerteint">
+            &nbsp;
+          </a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
